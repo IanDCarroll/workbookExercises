99008db9f5b3f94a786c0293e0a31f1a9e047857
IanDCarroll <ian.d.carroll@gmail.com>
2016-08-08 09:47:15 -0700

    went back and added ex2.py

---
+++ LPtHW/ex2.py
@@ -0,0 +1,21 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print "This will run."
+
+# ******************************
+
+# I avoided this excercise because I thought it was too boring 
+# and I didn't need it.
+# I was in a hurry to learn and thought I could skip this exercise.
+# In fact, I skipped quite a few exercises after this one, too.
+
+# Now, I'm going back through and making sure 
+# I didn't miss anything important.
+
+# It's an exercise in patience, presence, focus, and attention to detail.
