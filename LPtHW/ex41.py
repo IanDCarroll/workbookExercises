bb32449d4186b8b508ed1f806f64b184b1bda71b
IanDCarroll <ian.d.carroll@gmail.com>
2016-09-14 12:09:12 -0700

    add comments to ex41.py

--- LPtHW/ex41.py
+++ LPtHW/ex41.py
@@ -1,7 +1,11 @@
+# random is a library that gives access to python's pseudo-random number generator
+# and useful functions for manipulating it.
 import random
+# urllib is a libarary with calls to the internet
 from urllib import urlopen
 import sys
 
+# These all-caps are special variables that are useful to our import libraries
 WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
@@ -21,31 +25,47 @@ PHRASES = {
 }
 
 # do they want to drill phrases first
+# creates two conditions for how many argv parameters can be passed
 PHRASE_FIRST = False
 if len(sys.argv) == 2 and sys.argv[1] == "english":
     PHRASE_FIRST = True
 
 # load up the words from the website
 for word in urlopen(WORD_URL).readlines():
+    # with the imported words from the URL, first strip them of whitespace,
+    # then add them to the script's empty WORDS list.
+    # this is a way of parsing and loading data from a database.
     WORDS.append(word.strip())
 
 
 def convert(snippet, phrase):
+    # use a built-in string method to capitalize class names
     class_names = [w.capitalize() for w in 
                    random.sample(WORDS, snippet.count("%%%"))]
+    # uses the sample method from random, samples from our words list
+    # and takes only as many as are found in snippet.count()
+    # s.count() is a built-in string method which counts occurneces 
+    # of a substring in the string.
     other_names = random.sample(WORDS, snippet.count("***"))
     results = []
     param_names = []
 
     for i in range(0, snippet.count("@@@")):
+        # randint gives a random number between x and y.
+        # If these numbers are changed, the program will give different numbers
+        # of parameters. But the numbers are good as they are.
         param_count = random.randint(1,3)
+        # turns the list of param_names into a string
         param_names.append(', '.join(random.sample(WORDS, param_count)))
 
     for sentence in snippet, phrase:
+        #idiomatic way of copying a list
         result = sentence[:]
 
         # fake class names
         for word in class_names:
+            # s.replace is a built-in string method
+            # which replaces param 0 with param 1 param 2 rimes.
             result = result.replace("%%%", word, 1)
 
         # fake other names
@@ -65,6 +85,7 @@ def convert(snippet, phrase):
 try:
    while True:
        snippets = PHRASES.keys()
+       # another method of random
        random.shuffle(snippets)
 
        for snippet in snippets:
