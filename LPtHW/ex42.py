df02cedaf3b907ab64b5dadc8c32ed18496593d8
IanDCarroll <ian.d.carroll@gmail.com>
2016-09-15 11:28:42 -0700

    add comments to ex42.py

--- LPtHW/ex42.py
+++ LPtHW/ex42.py
@@ -2,48 +2,52 @@
 class Animal(object):
     pass
 
-## ??
+## Dog is an Animal
 class Dog(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## Dog has-a name
         self.name = name
 
-## ??
+## Cat is an animal
 class Cat(Animal):
 
     def __init_(self, name):
-        ## ??
+        ## Cat has-a name
         self.name = name
 
-## ??
+## Person is an object
 class Person(object):
 
     def __init__(self, name):
-        ## ??
+        ## Person has-a name
         self.name = name
 
         ## Person has-a pet of some kind
         self.pet = None
 
-## ??
+## Employee is-a Person
 class Employee(Person):
 
     def __init__(self, name, salary):
-        ## ?? hmm what is this strange magic?
+        ## In a class hierarchy with single inheritance, super can be used
+        # to refer to parent classes without naming them explicitly, 
+        # thus making the code more maintainable. This use closely parallels 
+        # the use of super in other programming languages.
+        # https://docs.python.org/2/library/functions.html#super
         super(Employee, self).__init__(name)
-        ## ??
+        ## Employee has-a salary
         self.salary = salary
 
-## ??
+## Fish is an object
 class Fish(object):
     pass
 
-## ??
+## Salmon is-a fish
 class Salmon(Fish):
     pass
 
-## ??
+## Halibut is-a fish
 class Halibut(Fish):
     pass
 
@@ -51,26 +55,26 @@ class Halibut(Fish):
 ## rover is-a Dog
 rover = Dog("Rover")
 
-## ??
+## satan is-a Cat
 satan = Cat("Satan")
 
-## ??
+## mary is-a Person
 mary = Person("Mary")
 
-## ??
+## mary has-a pet that is-a Cat named satan
 mary.pet = satan
 
-## ??
+## frank is an Employee
 frank = Employee("Frank", 120000)
 
-## ??
+## frank has a pet that is-a Dog named rover
 frank.pet = rover
 
-## ??
+## flipper is-a Fish (this flipper is not a dolphin. Dolphins are not fish.)
 flipper = Fish()
 
-## ??
+## crouse is-a Salmon
 crouse = Salmon()
 
-## ??
+## harry is-a Halibut
 harry = Halibut()
