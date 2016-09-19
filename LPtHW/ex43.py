dd8c486c2a15c0d374564e364c21c8439de9a906
IanDCarroll <ian.d.carroll@gmail.com>
2016-09-19 12:56:20 -0700

    add Jenny's back door to armory in ex43.py

--- LPtHW/ex43.py
+++ LPtHW/ex43.py
@@ -105,18 +105,18 @@ class LaserWeaponArmory(Scene):
         print """
         You do a dive roll into the Weapon Armory, crouch and scan the room
         for more Gothons that might be hiding.  it's dead quiet, too quiet.
-        You stand up and run to the far side of the room and find the neutron
-        bomb in its container. There's a keypad lock on the box and you need
-        the code to get the bomb out.  If you get the code wrong 10 times then
-        the lock closes forever and you can't get the bomb. 
-        The code is 3 digits.
+        You stand up and run to the far side of the room and find "Jenny",
+        the ship's neutron bomb in its container. There's a keypad lock on the 
+        box and you need the code to get the bomb out.  If you get the code 
+        wrong 10 times then the lock closes forever and you can't get the bomb. 
+        The key pad says "Enter the 3 digit code."
         """
 
         code = "%d%d%d" % (randint(1,9),randint(1,9),randint(1,9)) 
         guess = raw_input("[Keypad]> ")
         guesses = 0
-
-        while guess != code and guesses < 10:
+        
+        while guess != code and guess != '8675309' and guesses < 10:
             print "BZZZZEDDD!"
             guesses += 1
             guess = raw_input("[Keypad]> ")
@@ -126,6 +126,14 @@ class LaserWeaponArmory(Scene):
         The container clicks open and the seal breaks, letting gas out.
         You grab the neutron bomb and run as fast as you can to the
         bridge where you must place it in the right spot.
+            """
+            return 'the_bridge'
+        elif guess == '8675309':
+            print """
+        You got her number on the wall! The keypad was a liar, simply to fool
+        those who just don't understand late 20th century classical music. Thank
+        goodness you are so cultured as to know. You take Jenny up to the bridge
+        for a good time...
             """
             return 'the_bridge'
         else:
