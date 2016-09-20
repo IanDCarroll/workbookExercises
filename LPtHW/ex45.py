a11aa70b8883c3a03d92b8bca240334eed333707
IanDCarroll <ian.d.carroll@gmail.com>
2016-09-19 18:13:45 -0700

    initial commit ex45.py garden game spec

---
+++ LPtHW/ex45.py
@@ -0,0 +1,6 @@
+# 1. Make a different game than the others.
+# 2. Use more than one file. Use imports to make it happen.
+# 3. Use one class per room and give the rooms names that fit the description.
+# 4. The engine needs to know the rooms and how to navigate them.
+
+# The Garden of The Harvest Moon : A relaxing chinese garden game.
