df5d400ff693e29592901cb7a9d4b8ff2141c4ca
IanDCarroll <ian.d.carroll@gmail.com>
2016-09-24 23:23:00 -0700

    pass first part of first test ex48

--- LPtHW/ex48/ex48/lexicon.py
+++ LPtHW/ex48/ex48/lexicon.py
@@ -1,2 +1,12 @@
 def scan(string):
-    pass
+    string.split(' ')
+    index = [('direction', 'north'),
+             ('direction', 'south'),
+             ('direction', 'east')]
+    # word in string returns the string, not the index
+    for word in string:
+        for i in range(0, len(index)):
+            if word in index[i][1]:
+                return [index[i]]
+            else:
+                return "no"
