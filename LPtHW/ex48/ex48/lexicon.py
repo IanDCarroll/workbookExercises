abd836d78667e5bae5a426e6527d20318dfafe94
IanDCarroll <ian.d.carroll@gmail.com>
2016-09-25 19:40:03 -0700

    abstract index to global variable in ex48

--- LPtHW/ex48/ex48/lexicon.py
+++ LPtHW/ex48/ex48/lexicon.py
@@ -1,8 +1,15 @@
+
+
+index = [('direction', 'north'),
+         ('direction', 'south'),
+         ('direction', 'east')]
+
+
 def scan(string):
+    global index
+
     string.split(' ')
-    index = [('direction', 'north'),
-             ('direction', 'south'),
-             ('direction', 'east')]
+
     # word in string returns the string, not the index
     for word in string:
         for i in range(0, len(index)):
