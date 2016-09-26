62e12ae9fa87a7181b39ccdbe5c315ca01cb1683
IanDCarroll <ian.d.carroll@gmail.com>
2016-09-26 06:17:27 -0700

    include more words and test coverage ex48

--- LPtHW/ex48/tests/ex48_tests.py
+++ LPtHW/ex48/tests/ex48_tests.py
@@ -7,10 +7,17 @@ def test_directions():
     assert_equal(result, [('direction', 'north'),
                           ('direction', 'south'),
                           ('direction', 'east')])
+    result = lexicon.scan("back right left down up")
+    assert_equal(result, [('direction', 'back'),
+                           ('direction', 'right'),
+                           ('direction', 'left'),
+                           ('direction', 'down'),
+                           ('direction', 'up')])
 
 
 def test_verbs():
     assert_equal(lexicon.scan("go"), [('verb', 'go')])
+    assert_equal(lexicon.scan("stop"), [('verb', 'stop')])
     result = lexicon.scan("go kill eat")
     assert_equal(result, [('verb', 'go'),
                           ('verb', 'kill'),
@@ -23,6 +30,10 @@ def test_stops():
     assert_equal(result, [('stop', 'the'),
                           ('stop', 'in'),
                           ('stop', 'of')])
+    result = lexicon.scan("from at it")
+    assert_equal(result, [('stop', 'from'),
+                          ('stop', 'at'),
+                          ('stop', 'it')])
 
 
 def test_nouns():
@@ -30,6 +41,9 @@ def test_nouns():
     result = lexicon.scan("bear princess")
     assert_equal(result, [('noun', 'bear'),
                           ('noun', 'princess')])
+    result = lexicon.scan("cabinet door")
+    assert_equal(result, [('noun', 'cabinet'),
+                          ('noun', 'door')])
 
 
 def test_numbers():
