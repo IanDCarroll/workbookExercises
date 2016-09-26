8522de493e83d099b5a15d428bda1db9ebb348a9
IanDCarroll <ian.d.carroll@gmail.com>
2016-09-26 13:17:02 -0700

    include standard test w/o exception raising ex48

--- LPtHW/ex48/tests/parser_tests.py
+++ LPtHW/ex48/tests/parser_tests.py
@@ -10,3 +10,50 @@ def test_parser_no_subject():
     assert_equal(result1, 'player')
     assert_equal(result2, 'go')
     assert_equal(result3, 'north')
+
+def test_parser_with_stops():
+    theCall = parser.parse_sentence([('verb', 'kill'), 
+                                     ('stop', 'the'),
+                                     ('noun', 'princess')])
+    result1 = getattr(theCall, 'subject')
+    result2 = getattr(theCall, 'verb')
+    result3 = getattr(theCall, 'object')
+
+    assert_equal(result1, 'player')
+    assert_equal(result2, 'kill')
+    assert_equal(result3, 'princess')
+
+def test_parser_with_subject():
+    theCall = parser.parse_sentence([('stop', 'the'), 
+                                     ('noun', 'bear'),
+                                     ('verb', 'go'),
+                                     ('stop', 'in'),
+                                     ('stop', 'the'),                                  
+                                     ('noun', 'cabinet')])
+    result1 = getattr(theCall, 'subject')
+    result2 = getattr(theCall, 'verb')
+    result3 = getattr(theCall, 'object')
+
+    assert_equal(result1, 'bear')
+    assert_equal(result2, 'go')
+    assert_equal(result3, 'cabinet')
+
+def test_parser_with_extras():
+    theCall = parser.parse_sentence([('verb', 'open'), 
+                                     ('stop', 'the'),
+                                     ('noun', 'door'),
+                                     ('error', 'and'),
+                                     ('error', 'smack'),
+                                     ('stop', 'the'),
+                                     ('noun', 'bear'),                                  
+                                     ('stop', 'in'),                                  
+                                     ('stop', 'the'),                                  
+                                     ('error', 'nose')])
+    result1 = getattr(theCall, 'subject')
+    result2 = getattr(theCall, 'verb')
+    result3 = getattr(theCall, 'object')
+
+    assert_equal(result1, 'player')
+    assert_equal(result2, 'open')
+    assert_equal(result3, 'door')
+
