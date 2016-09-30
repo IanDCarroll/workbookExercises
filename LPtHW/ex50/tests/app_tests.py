9de995c07d4f28aa105d3c12d9ad41a19d6ae8bb
IanDCarroll <ian.d.carroll@gmail.com>
2016-09-30 01:52:01 -0700

    "complete" ex52

--- LPtHW/ex50/tests/app_tests.py
+++ LPtHW/ex50/tests/app_tests.py
@@ -5,17 +5,22 @@ from tests.tools import assert_response
 def test_index():
     # check that we get a 404 on the / URL
     resp = app.request("/")
-    assert_response(resp, status="404")
+    assert_response(resp, status="303")
 
+def test_game():
     # test our first GET request to /hello
-    resp = app.request("/hello")
+    resp = app.request("/game")
     assert_response(resp)
 
-    # make sure default values work for the form
-    resp = app.request("/hello", method="POST")
-    assert_response(resp, contains="Nobody")
+# Old tests whose basic structure may be useful in the future #
 
+#def test_input():
+    # make sure default values work for the form
+#    resp = app.request("/hello", method="POST")
+#    assert_response(resp, contains="Nobody")
+
+#def test_input2():
     # test that we get expected values
-    data = {'name': 'Zed', 'greet': 'Hola'}
-    resp = app.request("/hello", method="POST", data=data)
-    assert_response(resp, contains="Zed")
+#    data = {'name': 'Zed', 'greet': 'Hola'}
+#    resp = app.request("/hello", method="POST", data=data)
+#    assert_response(resp, contains="Zed")
