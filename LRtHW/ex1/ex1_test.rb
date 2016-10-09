37c2d124e455d08ffac2d6954152db025064c20e
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-09 12:39:12 -0700

    add line-by-line tests to easily pin-point defects

--- LRtHW/ex1/ex1_test.rb
+++ LRtHW/ex1/ex1_test.rb
@@ -7,6 +7,28 @@ class TestBlog < Minitest::Test
     @ex1 = Ex1.new
   end
 
+  def test_line_1
+    assert_output(/Hello World!\n/) { @ex1.firstProgram }
+  end
+  def test_line_2
+    assert_output(/Hello Again\n/) { @ex1.firstProgram }
+  end
+  def test_line_3
+    assert_output(/I like typing this.\n/) { @ex1.firstProgram }
+  end
+  def test_line_4
+    assert_output(/This is fun.\n/) { @ex1.firstProgram }
+  end
+  def test_line_5
+    assert_output(/Yay! Printing.\n/) { @ex1.firstProgram }
+  end
+  def test_line_6
+    assert_output(/I'd much rather you 'not'.\n/) { @ex1.firstProgram }
+  end
+  def test_line_7
+    assert_output(/I "said" do not touch this.\n/) { @ex1.firstProgram }
+  end
+
   def test_lines
     assert_output(
 /^Hello World!
