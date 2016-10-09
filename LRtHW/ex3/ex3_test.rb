84055ee34754a42adb2fbaeef864d02948edded6
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-09 16:20:20 -0700

    delete test_test, make test_line_13 more robust

--- LRtHW/ex3/ex3_test.rb
+++ LRtHW/ex3/ex3_test.rb
@@ -8,56 +8,43 @@ class Ex3Test < Minitest::Test
   end
 
   def test_line_1
-    assert_output(/I will now count my chickens/) { @ex3.math }
+    assert_output(/^I will now count my chickens.\n/) { @ex3.math }
   end
   def test_line_2
-    assert_output(/Hens 30/) { @ex3.math }
+    assert_output(/Hens 30\n/) { @ex3.math }
   end
   def test_line_3
-    assert_output(/Roosters 97/) { @ex3.math }
+    assert_output(/Roosters 97\n/) { @ex3.math }
   end
   def test_line_4
-    assert_output(/Now I will count the eggs:/) { @ex3.math }
+    assert_output(/Now I will count the eggs:\n/) { @ex3.math }
   end
-#  def test_line_6
-#    assert_output(/Is it true that 3 + 2 < 5 - 7?/) { @ex3.math }
-#  end
-#  def test_line_8
-#    assert_output(/What is 3 + 2? 5/) { @ex3.math }
-#  end
-#  def test_line_9
-#    assert_output(/What is 5 - 7? -2/) { @ex3.math }
-#  end
+  def test_line_6
+    assert_output(/Is it true that 3 \+ 2 < 5 - 7\?/) { @ex3.math }
+  end
+  def test_line_8
+    assert_output(/What is 3 \+ 2\? 5\n/) { @ex3.math }
+  end
+  def test_line_9
+    assert_output(/What is 5 - 7\? -2\n/) { @ex3.math }
+  end
   def test_line_10
-    assert_output(/Oh, that's why it's false./) { @ex3.math }
+    assert_output(/Oh, that's why it's false.\n/) { @ex3.math }
   end
   def test_line_11
-    assert_output(/How about some more./) { @ex3.math }
+    assert_output(/How about some more.\n/) { @ex3.math }
   end
-#  def test_line_12
-#    assert_output(/Is it greater? true/) { @ex3.math }
-#  end
-#  def test_line_13
-#    assert_output(/Is it greater or equal? true/) { @ex3.math }
-#  end
+  def test_line_12
+    assert_output(/Is it greater\? true\n/) { @ex3.math }
+  end
+  def test_line_13
+    assert_output(/Is it greater or equal\? true\n.{26}\n$/) { @ex3.math }
+  end
   def test_line_14
-    assert_output(/Is it less or equal\? false/) { @ex3.math }
+    assert_output(/Is it less or equal\? false\n$/) { @ex3.math }
   end
 #  def test_text
-#    assert_output(/^I will now count my chickens:
-#Hens 30
-#Roosters 97
-#Now I will count the eggs:
-#7
-#Is it true that 3 + 2 < 5 - 7?
-#false
-#what is 3 + 2? 5
-#what is 5 - 7? -2
-#Oh, that's why it's false.
-#How about some more.
-#Is it greater? true
-#Is it greater or equal? true
-#Is it less or equal? false\n$/) { @ex3.math }
+#    assert_output(/^I will now count my chickens:\nHens 30\nRoosters 97\nNow I will count the eggs:\n7\nIs it true that 3 \+ 2 < 5 - 7\?\nfalse\nWhat is 3 \+ 2\? 5\nWhat is 5 - 7\? -2\nOh, that's why it's false\.\nHow about some more\.\nIs it greater\? true\nIs it greater or equal\? true\nIs it less or equal\? false\n$/) { @ex3.math }
 #  end
 
 end
