5412d0b1e8bd00af724c8a495dab823b4b33a5f6
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-09 12:11:52 -0700

    complete test case

--- LRtHW/ex1/ex1_test.rb
+++ LRtHW/ex1/ex1_test.rb
@@ -8,7 +8,7 @@ class TestBlog < Minitest::Test
   end
 
   def test_lines
-    assert_output(/Hello World!\nHello Again/) { @ex1.firstProgram }
+    assert_output(/Hello World!\nHello Again\nI like typing this.\nThis is fun.\nYay! Printing.\nI'd much rather you 'not'.\nI "said" do not tough this.\n/) { @ex1.firstProgram }
   end
 
 end
