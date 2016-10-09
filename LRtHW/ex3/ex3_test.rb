48343d6fe8a95c5ad593e972fc17fdad8d1c807a
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-09 16:57:23 -0700

    use regex to make tests specific

--- LRtHW/ex3/ex3_test.rb
+++ LRtHW/ex3/ex3_test.rb
@@ -8,34 +8,40 @@ class Ex3Test < Minitest::Test
   end
 
   def test_line_1
-    assert_output(/^I will now count my chickens.\n/) { @ex3.math }
+    assert_output(/^I will now count my chickens\.\n/) { @ex3.math }
   end
   def test_line_2
-    assert_output(/Hens 30\n/) { @ex3.math }
+    assert_output(/^.{29}\nHens 30\n/) { @ex3.math }
   end
   def test_line_3
-    assert_output(/Roosters 97\n/) { @ex3.math }
+    assert_output(/^.{29}\n.{7}\nRoosters 97\n/) { @ex3.math }
   end
   def test_line_4
-    assert_output(/Now I will count the eggs:\n/) { @ex3.math }
+    assert_output(/^.{29}\n.{7}\n.{11}\nNow I will count the eggs:\n/) { @ex3.math }
+  end
+  def test_line_5
+    assert_output(/^.{29}\n.{7}\n.{11}\n.{26}\n7\n/) { @ex3.math }
   end
   def test_line_6
-    assert_output(/Is it true that 3 \+ 2 < 5 - 7\?/) { @ex3.math }
+    assert_output(/^.{29}\n.{7}\n.{11}\n.{26}\n.\nIs it true that 3 \+ 2 < 5 - 7\?/) { @ex3.math }
+  end
+  def test_line_7
+    assert_output(/^.{29}\n.{7}\n.{11}\n.{26}\n.\n.{30}\nfalse\n/) { @ex3.math }
   end
   def test_line_8
-    assert_output(/What is 3 \+ 2\? 5\n/) { @ex3.math }
+    assert_output(/What is 3 \+ 2\? 5\n.{17}\n.{26}\n.{20}\n.{19}\n.{28}\n.{26}\n$/) { @ex3.math }
   end
   def test_line_9
-    assert_output(/What is 5 - 7\? -2\n/) { @ex3.math }
+    assert_output(/What is 5 - 7\? -2\n.{26}\n.{20}\n.{19}\n.{28}\n.{26}\n$/) { @ex3.math }
   end
   def test_line_10
-    assert_output(/Oh, that's why it's false.\n/) { @ex3.math }
+    assert_output(/Oh, that's why it's false\.\n.{20}\n.{19}\n.{28}\n.{26}\n$/) { @ex3.math }
   end
   def test_line_11
-    assert_output(/How about some more.\n/) { @ex3.math }
+    assert_output(/How about some more\.\n.{19}\n.{28}\n.{26}\n$/) { @ex3.math }
   end
   def test_line_12
-    assert_output(/Is it greater\? true\n/) { @ex3.math }
+    assert_output(/Is it greater\? true\n.{28}\n.{26}\n$/) { @ex3.math }
   end
   def test_line_13
     assert_output(/Is it greater or equal\? true\n.{26}\n$/) { @ex3.math }
