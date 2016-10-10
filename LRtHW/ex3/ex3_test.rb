439c0b09e7816b9d37951c6424688e6b68db212e
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-09 17:30:20 -0700

    full test coverage for text + positioning

--- LRtHW/ex3/ex3_test.rb
+++ LRtHW/ex3/ex3_test.rb
@@ -7,46 +7,49 @@ class Ex3Test < Minitest::Test
     @ex3 = Ex3.new()
   end
 
-  def test_line_1
+  def test_line_1_position
     assert_output(/^I will now count my chickens\.\n/) { @ex3.math }
   end
-  def test_line_2
+#  def test_line_1_existence
+#    assert_output(/I will now count my chickens\./) { @ex3.math }
+#  end
+  def test_line_2_position
     assert_output(/^.*\nHens 30\n/) { @ex3.math }
   end
-  def test_line_3
-    assert_output(/^.*\n.*\nRoosters 97\n/) { @ex3.math }
+  def test_line_3_position
+    assert_output(/^(.*\n){2}Roosters 97\n/) { @ex3.math }
   end
-  def test_line_4
-    assert_output(/^.*\n.*\n.*\nNow I will count the eggs:\n/) { @ex3.math }
+  def test_line_4_position
+    assert_output(/^(.*\n){3}Now I will count the eggs:\n/) { @ex3.math }
   end
-  def test_line_5
-    assert_output(/^.*\n.*\n.*\n.*\n7\n/) { @ex3.math }
+  def test_line_5_position
+    assert_output(/^(.*\n){4}7\n/) { @ex3.math }
   end
-  def test_line_6
-    assert_output(/^.*\n.*\n.*\n.*\n.*\nIs it true that 3 \+ 2 < 5 - 7\?\n/) { @ex3.math }
+  def test_line_6_position
+    assert_output(/^(.*\n){5}Is it true that 3 \+ 2 < 5 - 7\?\n/) { @ex3.math }
   end
-  def test_line_7
-    assert_output(/^.*\n.*\n.*\n.*\n.*\n.*\nfalse\n/) { @ex3.math }
+  def test_line_7_position
+    assert_output(/^(.*\n){6}false\n(.*\n){7}$/) { @ex3.math }
   end
-  def test_line_8
-    assert_output(/What is 3 \+ 2\? 5\n.*\n.*\n.*\n.*\n.*\n.*\n$/) { @ex3.math }
+  def test_line_8_position
+    assert_output(/^(.*\n){7}What is 3 \+ 2\? 5\n(.*\n){6}$/) { @ex3.math }
   end
-  def test_line_9
-    assert_output(/What is 5 - 7\? -2\n.*\n.*\n.*\n.*\n.*\n$/) { @ex3.math }
+  def test_line_9_position
+    assert_output(/What is 5 - 7\? -2\n(.*\n){5}$/) { @ex3.math }
   end
-  def test_line_10
-    assert_output(/Oh, that's why it's false\.\n.*\n.*\n.*\n.*\n$/) { @ex3.math }
+  def test_line_10_position
+    assert_output(/Oh, that's why it's false\.\n(.*\n){4}$/) { @ex3.math }
   end
-  def test_line_11
-    assert_output(/How about some more\.\n.*\n.*\n.*\n$/) { @ex3.math }
+  def test_line_11_position
+    assert_output(/How about some more\.\n(.*\n){3}$/) { @ex3.math }
   end
-  def test_line_12
-    assert_output(/Is it greater\? true\n.*\n.*\n$/) { @ex3.math }
+  def test_line_12_position
+    assert_output(/Is it greater\? true\n(.*\n){2}$/) { @ex3.math }
   end
-  def test_line_13
+  def test_line_13_position
     assert_output(/Is it greater or equal\? true\n.*\n$/) { @ex3.math }
   end
-  def test_line_14
+  def test_line_14_position
     assert_output(/Is it less or equal\? false\n$/) { @ex3.math }
   end
 
