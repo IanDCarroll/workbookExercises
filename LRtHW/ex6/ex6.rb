69fe5e0e16963bd53cca56ad601ce48fd7400c1b
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-11 23:29:24 -0700

    test drive ex6.rb to pass all tests

--- LRtHW/ex6/ex6.rb
+++ LRtHW/ex6/ex6.rb
@@ -1,7 +1,27 @@
 class Ex6
 
   def strings_and_text
-    puts ""
+    types_of_people = 10
+    x = "There are #{types_of_people} types of people."
+    binary = "binary"
+    do_not = "don't"
+    y = "Those who know #{binary} and those who #{do_not}."
+
+    puts x
+    puts y
+
+    puts "I said: #{x}."
+    puts "I also said: '#{y}'."
+
+    hilarious = false
+    joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+    puts joke_evaluation
+
+    w = "This is the left side of..."
+    e = "a string with a right side."
+
+    puts w + e
   end
 end
 
