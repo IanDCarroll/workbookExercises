ab106a42ae6dce748312d32d16f1cb19e70574e7
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-11 23:16:36 -0700

    write tests for each line of ex6 output

--- LRtHW/ex6/ex6_test.rb
+++ LRtHW/ex6/ex6_test.rb
@@ -7,7 +7,25 @@ class Ex6Test < Minitest::Test
     @ex6 = Ex6.new()
   end
 
-  def test
-    assert_output(//) { @ex6.strings_and_text }
+  # simple tests that are very strict. No deviation! Ever! #
+
+  def test_line_
+    assert_output(/There are 10 types of people\.\n/) { @ex6.strings_and_text }
   end
+  def test_line_
+    assert_output(/Those who know binary and those who don't\.\n/) { @ex6.strings_and_text }
+  end
+  def test_line_
+    assert_output(/I said: There are 10 types of people\.\.\n/) { @ex6.strings_and_text }
+  end
+  def test_line_
+    assert_output(/I also said: 'Those who know binary and those who don't\.'\.\n/) { @ex6.strings_and_text }
+  end
+  def test_line_
+    assert_output(/Isn't that joke so funny\?! (false|true)\n/) { @ex6.strings_and_text }
+  end
+  def test_line_
+    assert_output(/This is the left side of\.\.\.a string with a right side\.\n/) { @ex6.strings_and_text }
+  end
+
 end
