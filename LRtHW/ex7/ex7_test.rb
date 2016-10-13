ffd15d919462f3cc55383b588d462925e5404740
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-13 13:17:39 -0700

    include line positions as part of ex7 tests

--- LRtHW/ex7/ex7_test.rb
+++ LRtHW/ex7/ex7_test.rb
@@ -7,20 +7,20 @@ class Ex7Test < Minitest::Test
     @ex7 = Ex7.new()
   end
 
-  def test_line_
-    assert_output(/Mary had a little lamb\./) { @ex7.more_printing }
+  def test_line_1
+    assert_output(/^Mary had a little lamb\.\n(.*\n){4}$/) { @ex7.more_printing }
   end
-  def test_line_
-    assert_output(/Its fleece was white as snow\./) { @ex7.more_printing }
+  def test_line_2
+    assert_output(/^.*\nIts fleece was white as snow\.\n(.*\n){3}$/) { @ex7.more_printing }
   end
-  def test_line_
-    assert_output(/And everywhere that Mary went\./) { @ex7.more_printing }
+  def test_line_3
+    assert_output(/^(.*\n){2}And everywhere that Mary went\.\n(.*\n){2}$/) { @ex7.more_printing }
   end
-  def test_line_
-    assert_output(/\.{10}/) { @ex7.more_printing }
+  def test_line_4
+    assert_output(/^(.*\n){3}\.{10}\n.*\n$/) { @ex7.more_printing }
   end
-  def test_line_
-    assert_output(/CheeseBurger/) { @ex7.more_printing }
+  def test_line_5
+    assert_output(/^(.*\n){4}CheeseBurger\n$/) { @ex7.more_printing }
   end
 
 end
