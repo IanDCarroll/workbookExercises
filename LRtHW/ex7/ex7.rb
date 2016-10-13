52f9ff4bdec71a0d8e5b81c67a30f4c983535e7b
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-13 13:39:10 -0700

    refactor line 5 to be more convoluted and ugly

--- LRtHW/ex7/ex7.rb
+++ LRtHW/ex7/ex7.rb
@@ -6,7 +6,21 @@ class Ex7
     puts "Its fleece was white as #{'snow'}."
     puts "And everywhere that Mary went."
     puts "." * 10
-    puts "CheeseBurger"
+
+    char1 = 'C'
+    char2 = 'h'
+    char3 = 'e'
+    char4 = 'e'
+    char5 = 's'
+    char6 = 'e'
+    char7 = 'B'
+    char8 = 'u'
+    char9 = 'r'
+    cha10 = 'g'
+    cha11 = 'e'
+    cha12 = 'r'
+
+    puts char1 + char2 + char3 + char4 + char5 + char6 + char7 + char8 + char9 + cha10 + cha11 + cha12
 
   end
 
