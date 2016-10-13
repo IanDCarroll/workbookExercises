fedd5a9c77ec9ecf75f6feb85db95bb3b5f69376
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-13 13:11:32 -0700

    add line existence testing for ex7

--- LRtHW/ex7/ex7_test.rb
+++ LRtHW/ex7/ex7_test.rb
@@ -8,7 +8,19 @@ class Ex7Test < Minitest::Test
   end
 
   def test_line_
-    assert_output(//) { @ex7.more_printing }
+    assert_output(/Mary had a little lamb\./) { @ex7.more_printing }
+  end
+  def test_line_
+    assert_output(/Its fleece was white as snow\./) { @ex7.more_printing }
+  end
+  def test_line_
+    assert_output(/And everywhere that Mary went\./) { @ex7.more_printing }
+  end
+  def test_line_
+    assert_output(/\.{10}/) { @ex7.more_printing }
+  end
+  def test_line_
+    assert_output(/CheeseBurger/) { @ex7.more_printing }
   end
 
 end
