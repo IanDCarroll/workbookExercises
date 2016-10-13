cc503ab800abea7dbc40e0ff0cb30f873bce4c80
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-13 13:32:59 -0700

    refactor line 2 to include fomatting 'snow'

--- LRtHW/ex7/ex7.rb
+++ LRtHW/ex7/ex7.rb
@@ -3,7 +3,7 @@ class Ex7
   def more_printing
 
     puts "Mary had a little lamb."
-    puts "Its fleece was white as snow."
+    puts "Its fleece was white as #{'snow'}."
     puts "And everywhere that Mary went."
     puts ".........."
     puts "CheeseBurger"
