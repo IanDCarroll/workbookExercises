feab3b445102d22b907b80c5cd64e77050bb89a0
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-17 11:08:50 -0700

    refactor ex8 test script for positioning

--- LRtHW/ex8/ex8_test.rb
+++ LRtHW/ex8/ex8_test.rb
@@ -8,19 +8,19 @@ class Ex8Test < Minitest::Test
   end
 
   def test_line_1
-    assert_output(/1 2 3 4/) { @ex8.printingX2 }
+    assert_output(/^1 2 3 4\n(.*\n){4}$/) { @ex8.printingX2 }
   end
   def test_line_2
-    assert_output(/one two three four/) { @ex8.printingX2 }
+    assert_output(/^.*\none two three four\n(.*\n){3}$/) { @ex8.printingX2 }
   end
   def test_line_3
-    assert_output(/true false true false/) { @ex8.printingX2 }
+    assert_output(/^(.*\n){2}true false true false\n(.*\n){2}$/) { @ex8.printingX2 }
   end
   def test_line_4
-    assert_output(/(%{first} %{second} %{third} %{fourth} ){3}%{first} %{second} %{third} %{fourth}/) { @ex8.printingX2 }
+    assert_output(/^(.*\n){3}(%{first} %{second} %{third} %{fourth} ){3}%{first} %{second} %{third} %{fourth}\n.*\n$/) { @ex8.printingX2 }
   end
   def test_line_5
-    assert_output(/I had this thing. That you could type up right. But it didn't sing. So I said goodnight./) { @ex8.printingX2 }
+    assert_output(/^(.*\n){4}I had this thing\. That you could type up right\. But it didn't sing\. So I said goodnight\.\n$/) { @ex8.printingX2 }
   end
 
 end
