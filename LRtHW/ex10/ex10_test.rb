526ce96d051638b7fb1c78c2e64d769c84470f45
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-17 13:57:08 -0700

    refactor test script for positioning

--- LRtHW/ex10/ex10_test.rb
+++ LRtHW/ex10/ex10_test.rb
@@ -8,34 +8,34 @@ class Ex10Test < Minitest::Test
   end
 
   def test_line_1
-    assert_output(/\tI'm tabbed in\.\n/) { @ex10.whatWasThat }
+    assert_output(/^\tI'm tabbed in\.\n(.*\n){8}.+$/) { @ex10.whatWasThat }
   end
   def test_line_2
-    assert_output(/I'm split\n/) { @ex10.whatWasThat }
+    assert_output(/^.*\nI'm split\n(.*\n){7}.+$/) { @ex10.whatWasThat }
   end
   def test_line_3
-    assert_output(/on a line\./) { @ex10.whatWasThat }
+    assert_output(/^(.*\n){2}on a line\.\n(.*\n){6}.+$/) { @ex10.whatWasThat }
   end
   def test_line_4
-    assert_output(/I'm  a  cat./) { @ex10.whatWasThat }
+    assert_output(/^(.*\n){3}I'm  a  cat\.\n(.*\n){5}.+$/) { @ex10.whatWasThat }
   end
   def test_line_5
-    assert_output(/\n/) { @ex10.whatWasThat }
+    assert_output(/^(.*\n){4}\n(.*\n){4}.+$/) { @ex10.whatWasThat }
   end
   def test_line_6
-    assert_output(/I'll do a list:\n/) { @ex10.whatWasThat }
+    assert_output(/^(.*\n){5}I'll do a list:\n(.*\n){3}.+$/) { @ex10.whatWasThat }
   end
   def test_line_7
-    assert_output(/\t\* Cat food\n/) { @ex10.whatWasThat }
+    assert_output(/^(.*\n){6}\t\* Cat food\n(.*\n){2}.+$/) { @ex10.whatWasThat }
   end
   def test_line_8
-    assert_output(/\t\* Fishes\n/) { @ex10.whatWasThat }
+    assert_output(/^(.*\n){7}\t\* Fishes\n.*\n.+$/) { @ex10.whatWasThat }
   end
   def test_line_9
-    assert_output(/\t\* Catnip\n/) { @ex10.whatWasThat }
+    assert_output(/^(.*\n){8}\t\* Catnip\n.+$/) { @ex10.whatWasThat }
   end
   def test_line_10
-    assert_output(/\t\* Grass\n/) { @ex10.whatWasThat }
+    assert_output(/^(.*\n){9}\t\* Grass$/) { @ex10.whatWasThat }
   end
 
 end
