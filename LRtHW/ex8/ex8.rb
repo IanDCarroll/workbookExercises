674ea432e8e855bcf57615e65434d8902a1c70e9
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-17 10:27:18 -0700

    refactor line 2

--- LRtHW/ex8/ex8.rb
+++ LRtHW/ex8/ex8.rb
@@ -5,7 +5,7 @@ class Ex8
     formatter = '%{first} %{second} %{third} %{fourth}'
     
     puts formatter % { first: 1, second: 2, third: 3, fourth: 4}
-    puts 'one two three four'
+    puts formatter % {first: 'one', second: 'two', third: 'three', fourth: 'four'}
     puts 'true false true false'
     puts '%{first} %{second} %{third} %{fourth}' * 4
     puts 'I had this thing. That you could type up right. But it didn\'t sing. So I said goodnight.'
