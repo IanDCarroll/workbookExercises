d8ef8fa73626749c7d31273700fc0f67699094ed
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-17 10:21:18 -0700

    passes all tests - ex8.rb

--- LRtHW/ex8/ex8.rb
+++ LRtHW/ex8/ex8.rb
@@ -5,6 +5,7 @@ class Ex8
     puts 'one two three four'
     puts 'true false true false'
     puts '%{first} %{second} %{third} %{fourth}' * 4
+    puts 'I had this thing. That you could type up right. But it didn\'t sing. So I said goodnight.'
 
   end
 
