2864423e84e6955e482a5017f69e44ba116d7478
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-17 13:05:25 -0700

    refactor ex9 output line 2-9

--- LRtHW/ex9/ex9.rb
+++ LRtHW/ex9/ex9.rb
@@ -3,9 +3,10 @@ class Ex9
   def printingX3
 
     days = "Mon Tue Wed Thu Fri Sat Sun"
+    months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
     puts "Here are the days: #{days}"
-    puts "Here are the months: Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+    puts "Here are the months: #{months}"
     puts """
 There's something going on here.
 With the three double-quotes.
