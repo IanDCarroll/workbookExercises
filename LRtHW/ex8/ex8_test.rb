cd9912a33901e61b4b8217f6d0b87cc09ca1b976
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-17 09:48:37 -0700

    complete initial test script

--- LRtHW/ex8/ex8_test.rb
+++ LRtHW/ex8/ex8_test.rb
@@ -7,8 +7,20 @@ class Ex8Test < Minitest::Test
     @ex8 = Ex8.new()
   end
 
-  def test
-    assert_output(//) { @ex8.printingX2 }
+  def test_line_1
+    assert_output(/1 2 3 4/) { @ex8.printingX2 }
+  end
+  def test_line_2
+    assert_output(/one two three four/) { @ex8.printingX2 }
+  end
+  def test_line_3
+    assert_output(/true false true false/) { @ex8.printingX2 }
+  end
+  def test_line_4
+    assert_output(/(%{first} %{second} %{third} %{fourth}){4}/) { @ex8.printingX2 }
+  end
+  def test_line_5
+    assert_output(/I had this thing. That you could type up right. But it didn't sing. So I said goodnight./) { @ex8.printingX2 }
   end
 
 end
