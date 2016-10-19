09f0905a23bb9c2c6a0f8484d7057985b545c5fe
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-19 09:12:08 -0700

    refactor test

--- LRtHW/ex11/ex11_test.rb
+++ LRtHW/ex11/ex11_test.rb
@@ -1,11 +1,12 @@
+require 'minitest/spec'
 require 'minitest/autorun'
 require './ex11'
 
 class Ex11Test < Minitest::Test
 
-  def setup
-    @ex11 = Ex11.new()
-  end
+#  def setup
+#    @ex11 = Ex11.new()
+#  end
 
   # ex11.questions needed to be seprated into its own function to test. 
   # Otherwise gets.chomp cloggs up the unit tests.
@@ -14,8 +15,20 @@ class Ex11Test < Minitest::Test
   # The trouble is I still have no way to test the connection between
   # qustions and answer because gets.chomp makes things difficult.
 
-  def test_answer
-    assert_output(/So, you're 33 years old, 66 inches tall and 155 lbs heavy\./) { @ex11.answer("33 years","66 inches","155 lbs") }
-  end
+#  def test_answer
+#    assert_output(/So, you're 33 years old, 66 inches tall and 155 lbs heavy\./) { @ex11.answer("33 years","66 inches","155 lbs") }
+#  end
 
+  describe "Answering questions" do
+    before do
+      @ex11 = Ex11.new()
+      def get_age; "33 years" end
+      def get_height; "66 inches" end
+      def get_weight; "155 lbs" end
+    end
+
+    it "should print" do
+      @ex11.answer.must_equal "So you're 33 years old, 66 inches tall and 155 lbs heavy."
+    end
+  end
 end
