bd895de039d0959b2c4e5cc8cf9148f518477cd5
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-19 08:53:37 -0700

    comment on testing with gets.chomp

--- LRtHW/ex11/ex11_test.rb
+++ LRtHW/ex11/ex11_test.rb
@@ -7,7 +7,14 @@ class Ex11Test < Minitest::Test
     @ex11 = Ex11.new()
   end
 
-  def test_line
+  # ex11.questions needed to be seprated into its own function to test. 
+  # Otherwise gets.chomp cloggs up the unit tests.
+  # Instead, I separate out the concerns, and mock the input of
+  # ex11.questions for ex11.answer.
+  # The trouble is I still have no way to test the connection between
+  # qustions and answer because gets.chomp makes things difficult.
+
+  def test_answer
     assert_output(/So, you're 33 years old, 66 inches tall and 155 lbs heavy\./) { @ex11.answer("33 years","66 inches","155 lbs") }
   end
 
