fd8910fda3fb8b6a17dc2a497f4c94755049de10
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-19 08:57:46 -0700

    refactor ex11.rb

--- LRtHW/ex11/ex11.rb
+++ LRtHW/ex11/ex11.rb
@@ -1,18 +1,29 @@
 class Ex11
 
-  def questions
+  def get_age
 
     puts "How old are you? "
     age = gets.chomp
+
+  end
+  def get_height
+
     puts "How tall are you? "
     height = gets.chomp
+
+  end
+  def get_weight
+
     puts "How much do you weigh? "
     weight = gets.chomp
-    return answer(age, height, weight)
 
   end
 
-  def answer(age, height, weight)
+  def answer
+
+    age = get_age
+    height = get_height
+    weight = get_weight
 
     puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
 
@@ -22,4 +33,4 @@ end
 
 ex11 = Ex11.new()
 
-ex11.questions()
+ex11.answer
