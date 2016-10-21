e9af92844b37f6785e7d147e59d09b248f899553
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-20 21:23:19 -0700

    initial ex13 script

---
+++ LRtHW/ex13/ex13.rb
@@ -0,0 +1,17 @@
+class Ex13
+
+  def argument_dept
+
+    first, second, third = ARGV
+
+    puts "Your first variable is: #{first}"
+    puts "Your second variable is: #{second}"
+    puts "Your third variable is: #{third}"
+
+  end
+
+end
+
+ex13 = Ex13.new()
+
+ex13.argument_dept
