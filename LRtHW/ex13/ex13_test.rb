5ee00903d3ee546e0030bb7841129327beaf877a
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-20 21:13:01 -0700

    inital test script for ex13

---
+++ LRtHW/ex13/ex13_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require './ex13'
+
+class Ex13Test < Minitest::Test
+
+  def setup
+    @ex13 = Ex13.new()
+  end
+
+  def test_line_1
+    assert_output(/^Your first variable is: .*\n.(+\n){2}$/) { @ex13.}
+  end
+  def test_line_2
+    assert_output(/^.+\nYour second variable is: .*\n.+\n$/) { @ex13.}
+  end
+  def test_line_3
+    assert_output(/^(.+\n){2}Your third variable is: .*\n$/) { @ex13.}
+  end
+
+end
