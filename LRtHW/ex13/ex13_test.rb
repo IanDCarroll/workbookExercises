2601ce752ef8feb7bcf572506ca65056b66e19c3
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-20 21:24:54 -0700

    fix part of test error message

--- LRtHW/ex13/ex13_test.rb
+++ LRtHW/ex13/ex13_test.rb
@@ -8,13 +8,13 @@ class Ex13Test < Minitest::Test
   end
 
   def test_line_1
-    assert_output(/^Your first variable is: .*\n.(+\n){2}$/) { @ex13.}
+    assert_output(/^Your first variable is: .*\n.(+\n){2}$/) { @ex13.argument_dept }
   end
   def test_line_2
-    assert_output(/^.+\nYour second variable is: .*\n.+\n$/) { @ex13.}
+    assert_output(/^.+\nYour second variable is: .*\n.+\n$/) { @ex13.argument_dept }
   end
   def test_line_3
-    assert_output(/^(.+\n){2}Your third variable is: .*\n$/) { @ex13.}
+    assert_output(/^(.+\n){2}Your third variable is: .*\n$/) { @ex13.argument_dept }
   end
 
 end
