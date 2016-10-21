a5140e2da217b66f3be31e40c724c5fb05b5a5af
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-20 20:21:59 -0700

    ensconce script in method & class then inst & call

--- LRtHW/ex12/ex12.rb
+++ LRtHW/ex12/ex12.rb
@@ -1,12 +1,24 @@
-print "Give me a number: "
-number = gets.chomp.to_i
+class Ex12
 
-bigger = number * 100
-puts "A bigger number is #{bigger}."
+  def prompt
 
-print "Give me another Number: "
-another = gets.chomp
-number = another.to_i
+    print "Give me a number: "
+    number = gets.chomp.to_i
 
-smaller = number / 100
-puts "A smaller number is #{smaller}"
+    bigger = number * 100
+    puts "A bigger number is #{bigger}."
+
+    print "Give me another Number: "
+    another = gets.chomp
+    number = another.to_i
+
+    smaller = number / 100
+    puts "A smaller number is #{smaller}"
+
+  end
+
+end
+
+ex12 = Ex12.new()
+
+ex12.prompt
