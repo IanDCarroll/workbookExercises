314fd0516ccad986394fbb763bbc705891c2ced8
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-30 19:27:39 -0700

    separated concerns in script ex14

--- LRtHW/ex14/ex14.rb
+++ LRtHW/ex14/ex14.rb
@@ -1,26 +1,52 @@
 class Ex14
 
-  def prompt
-    user_name = ARGV.first
-    prompt = '> '
+  def prompt_name
+    ARGV.first
+  end
+
+  def prompt_var
+    '> '
+  end
+
+  def prompt_likes
+    user_name = prompt_name
+    prompt = prompt_var
 
     puts "Hi #{user_name}."
     puts "I'd like to ask you a few questions."
     puts "Do you like me #{user_name}? ", prompt
-    likes = $stdin.gets.chomp
+    $stdin.gets.chomp
+  end
+
+  def prompt_lives
+    user_name = prompt_name
+    prompt = prompt_var
 
     puts "Where do you live #{user_name}? ", prompt
-    lives = $stdin.gets.chomp
+    $stdin.gets.chomp
+  end
+
+  def prompt_computer
+    prompt = prompt_var
 
     puts "What kind of computer do you have? ", prompt
-    computer = $stdin.gets.chomp
+    $stdin.gets.chomp
+  end
+
+  def prompt
+    likes = prompt_likes
+    lives = prompt_lives
+    computer = prompt_computer
 
     puts """
 Alright, So you said #{likes} about liking me.
 You live in #{lives}.  Not sure where that is.
 And you have a #{computer} computer.  Nice.
 """
-
   end
 
 end
+
+ex14 = Ex14.new()
+
+ex14.prompt
