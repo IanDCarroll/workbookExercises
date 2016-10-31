cf7d8516986f0e652a4074878c57767f59df070c
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-31 12:36:28 -0700

    create TestReader class to test the output of ex16

--- LRtHW/ex16/ex16_test.rb
+++ LRtHW/ex16/ex16_test.rb
@@ -1,14 +1,29 @@
 require 'minitest/autorun'
 require './ex16'
 
+class TestReader
+  def read_file
+    open('test.txt')
+  end
+  def print_file
+    print read_file.read
+  end
+end
+
 class Ex16Test < Minitest::Test
 
   def setup
-    @ex16 = Ex16.new
+    @reader = TestReader.new
   end
 
-  def test_something
-    assert_output(//) { @ex16.files }
+  def test_file_line_1
+    assert_output(/ancient pond\n/) { @reader.print_file }
+  end
+  def test_file_line_2
+    assert_output(/a frog jumps in\n/) { @reader.print_file }
+  end
+  def test_file_line_3
+    assert_output(/sound of water\n/) { @reader.print_file }
   end
 
 end
