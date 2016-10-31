ce56fa00e3339195a4b0262efe09f418fca3bb09
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-31 10:47:06 -0700

    create separate tests for two functions in ex15

--- LRtHW/ex15/ex15_test.rb
+++ LRtHW/ex15/ex15_test.rb
@@ -7,14 +7,23 @@ class Ex15Test < Minitest::Test
     @ex15 = Ex15.new()
   end
 
-  def test_file_output_line_1
-    assert_output(/^This is stuff I typed into a file\.\n(.*\n){2}$/) { @ex15.read }
+  def test_argv_output_line_1
+    assert_output(/^This is stuff I typed into a file\.\n(.*\n){2}$/) { @ex15.read_argv }
   end
-  def test_file_output_line_2
-    assert_output(/^.*\nIt is really cool stuff\.\n.*\n$/) { @ex15.read }
+  def test_argv_output_line_2
+    assert_output(/^.*\nIt is really cool stuff\.\n.*\n$/) { @ex15.read_argv }
   end
-  def test_file_output_line_3
-    assert_output(/^(.*\n){2}Lots and lots of fun to have in here\.\n$/) { @ex15.read }
+  def test_argv_output_line_3
+    assert_output(/^(.*\n){2}Lots and lots of fun to have in here\.\n$/) { @ex15.read_argv }
+  end
+  def test_gets_output_line_1
+    assert_output(/^This is stuff I typed into a file\.\n(.*\n){2}$/) { @ex15.read_gets }
+  end
+  def test_gets_output_line_2
+    assert_output(/^.*\nIt is really cool stuff\.\n.*\n$/) { @ex15.read_gets }
+  end
+  def test_gets_output_line_3
+    assert_output(/^(.*\n){2}Lots and lots of fun to have in here\.\n$/) { @ex15.read_gets }
   end
 
 end
