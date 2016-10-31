7e0c270acc37391399b39dd0c4621ac14b2fccf7
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-30 19:13:48 -0700

    wrap script in method prompt ex14

--- LRtHW/ex14/ex14.rb
+++ LRtHW/ex14/ex14.rb
@@ -1,23 +1,26 @@
 class Ex14
 
-  user_name = ARGV.first
-  prompt = '> '
+  def prompt
+    user_name = ARGV.first
+    prompt = '> '
 
-  puts "Hi #{user_name}."
-  puts "I'd like to ask you a few questions."
-  puts "Do you like me #{user_name}? ", prompt
-  likes = $stdin.gets.chomp
+    puts "Hi #{user_name}."
+    puts "I'd like to ask you a few questions."
+    puts "Do you like me #{user_name}? ", prompt
+    likes = $stdin.gets.chomp
 
-  puts "Where do you live #{user_name}? ", prompt
-  lives = $stdin.gets.chomp
+    puts "Where do you live #{user_name}? ", prompt
+    lives = $stdin.gets.chomp
 
-  puts "What kind of computer do you have? ", prompt
-  computer = $stdin.gets.chomp
+    puts "What kind of computer do you have? ", prompt
+    computer = $stdin.gets.chomp
 
-  puts """
+    puts """
 Alright, So you said #{likes} about liking me.
 You live in #{lives}.  Not sure where that is.
 And you have a #{computer} computer.  Nice.
 """
+
+  end
 
 end
