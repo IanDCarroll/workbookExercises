549621384f56d4259248987f16c163be4a3ae2bc
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-31 10:11:24 -0700

    create tests with expected text in ex15

--- LRtHW/ex15/ex15_test.rb
+++ LRtHW/ex15/ex15_test.rb
@@ -7,8 +7,14 @@ class Ex15Test < Minitest::Test
     @ex15 = Ex15.new()
   end
 
-  def test_something
-    assert_output(//) { @ex15.read }
+  def test_file_output_line_1
+    assert_output(/This is stuff I typed into a file\./) { @ex15.read }
+  end
+  def test_file_output_line_2
+    assert_output(/It is really cool stuff\./) { @ex15.read }
+  end
+  def test_file_output_line_3
+    assert_output(/Lots and lots of fun to have in here\./) { @ex15.read }
   end
 
 end
