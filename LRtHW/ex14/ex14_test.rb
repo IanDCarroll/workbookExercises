a19d2340936033ed2f88722161e3ed052fcff756
IanDCarroll <ian.d.carroll@gmail.com>
2016-10-30 18:55:08 -0700

    ex14 add regex to test script

--- LRtHW/ex14/ex14_test.rb
+++ LRtHW/ex14/ex14_test.rb
@@ -7,8 +7,14 @@ class Ex14Test < Minitest::Test
     @ex14 = Ex14.new()
   end
 
-  def test_something
-    assert_output(//) { @ex14.prompt }
+  def test_final_output_line_1
+    assert_output(/^Alright, so you said .* about liking me\.\n(.*\n){2}$/) { @ex14.prompt }
+  end
+  def test_final_output_line_2
+    assert_output(/^.*\nYou live in .*\.  Not sure where that is\.\n.*\n$/) { @ex14.prompt }
+  end
+  def test_final_output_line_3
+    assert_output(/^(.*\n){2}And you have a .* computer.  Nice\.\n$/) { @ex14.prompt }
   end
 
 end
