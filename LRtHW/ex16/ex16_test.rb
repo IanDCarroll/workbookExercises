aa306a40337d5ff687f822b404e5003573a800bf
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-02 11:06:43 -0700

    refactor TestReader in test script for ex16

--- LRtHW/ex16/ex16_test.rb
+++ LRtHW/ex16/ex16_test.rb
@@ -2,32 +2,41 @@ require 'minitest/autorun'
 require './ex16'
 
 class TestReader
-  def read_file
-    open('test.txt')
-  end
   def print_file
-    print read_file.read
+    print open('test.txt').read
   end
 end
 
 class Dummy < Ex16
-  
+  def open_file; open('test.txt', 'w') end
+  def get_new_lines
+    target = open_file
+    target.write('ancient pond')
+    target.write("\n")
+    target.write('a frog jumps in')
+    target.write("\n")
+    target.write('sound of water')
+    target.write("\n")
+  end
 end
 
 class Ex16Test < Minitest::Test
 
   def setup
+    #@dummy = Dummy.new
     @reader = TestReader.new
-    @dummy = Dummy.new
   end
 
   def test_file_line_1
+    #@dummy.writer
     assert_output(/^ancient pond\n(.*\n){2}$/) { @reader.print_file }
   end
   def test_file_line_2
+    #@dummy.writer
     assert_output(/^.*\na frog jumps in\n.*\n$/) { @reader.print_file }
   end
   def test_file_line_3
+    #@dummy.writer
     assert_output(/^(.*\n){2}sound of water\n$/) { @reader.print_file }
   end
 
