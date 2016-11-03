fd5e7620e0da26ae5c5855fcb68509129c079a5c
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-03 12:17:38 -0700

    further reduced puts statements in ex17

--- LRtHW/ex17/ex17.rb
+++ LRtHW/ex17/ex17.rb
@@ -1,8 +1,6 @@
 from_file, to_file = ARGV
 
-puts "Copying from #{from_file} to #{to_file}"
-
 in_file = open(from_file).read
 out_file = open(to_file, 'w').write(in_file)
 
-puts "Alright, all done."
+puts "Copied #{from_file} to #{to_file}"
