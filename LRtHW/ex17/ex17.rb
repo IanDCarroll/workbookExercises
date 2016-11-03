46f677e12f1e64e3dffbb01bf06bf6ed54f87950
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-03 12:15:34 -0700

    delete unnecessary puts and $stdin.gets in ex17

--- LRtHW/ex17/ex17.rb
+++ LRtHW/ex17/ex17.rb
@@ -3,13 +3,6 @@ from_file, to_file = ARGV
 puts "Copying from #{from_file} to #{to_file}"
 
 in_file = open(from_file).read
-
-puts "The input file is #{in_file.length} bytes long"
-
-puts "Does the output file exist? #{File.exist?(to_file)}"
-puts "Ready, hit RETURN to continue, CTRL-C to abort."
-$stdin.gets
-
 out_file = open(to_file, 'w').write(in_file)
 
 puts "Alright, all done."
