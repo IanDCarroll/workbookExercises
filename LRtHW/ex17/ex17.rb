464076a30fd79a3a787c06faa7bc03c8060d79ab
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-03 12:13:45 -0700

    further consolodate lines in ex17

--- LRtHW/ex17/ex17.rb
+++ LRtHW/ex17/ex17.rb
@@ -2,7 +2,6 @@ from_file, to_file = ARGV
 
 puts "Copying from #{from_file} to #{to_file}"
 
-# we could do these two on one line, how?
 in_file = open(from_file).read
 
 puts "The input file is #{in_file.length} bytes long"
@@ -11,9 +10,6 @@ puts "Does the output file exist? #{File.exist?(to_file)}"
 puts "Ready, hit RETURN to continue, CTRL-C to abort."
 $stdin.gets
 
-out_file = open(to_file, 'w')
-out_file.write(in_file)
+out_file = open(to_file, 'w').write(in_file)
 
 puts "Alright, all done."
-
-out_file.close
