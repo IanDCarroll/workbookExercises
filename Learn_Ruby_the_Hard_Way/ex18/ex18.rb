7d823ad0acdfaae3c7c8a09e281e10c43f28f7aa
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-16 12:24:18 -0800

    write third function

--- Learn_Ruby_the_Hard_Way/ex18/ex18.rb
+++ Learn_Ruby_the_Hard_Way/ex18/ex18.rb
@@ -11,5 +11,12 @@ def print_two_again(arg1, arg2)
   puts "arg1: #{arg1}, arg2: #{arg2}"
 end
 
+# this just prints one
+#    Is this all a monad is?
+def print_one(arg1)
+  puts "arg1: #{arg1}"
+end
+
 print_two("Zed","Shaw")
 print_two_again("Zed","Shaw")
+print_one("First!")
