b23ce300efaa3f27fb7a47a980e422c951d869b4
IanDCarroll <ian.d.carroll@gmail.com>
2016-11-16 12:27:42 -0800

    delete distracting comments

--- Learn_Ruby_the_Hard_Way/ex18/ex18.rb
+++ Learn_Ruby_the_Hard_Way/ex18/ex18.rb
@@ -1,23 +1,16 @@
-# this one is like your scripts with ARGV
 def print_two(*args)
   arg1, arg2 = args
   puts "arg1: #{arg1}, arg2: #{arg2}"
 end
 
-# ok, that *args is actually pointless, we can just do this
-#    But is it really pointless? 
-#    Could you use it to input a variable number of arguments?
 def print_two_again(arg1, arg2)
   puts "arg1: #{arg1}, arg2: #{arg2}"
 end
 
-# this just prints one
-#    Is this all a monad is?
 def print_one(arg1)
   puts "arg1: #{arg1}"
 end
 
-# this one takes no arguments
 def print_none()
   puts "I got nothin'."
 end
