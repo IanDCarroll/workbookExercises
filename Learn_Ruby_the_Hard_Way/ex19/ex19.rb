17b37bad2b8a0df33876a98a0e29ac88414b040f
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-22 21:38:18 -0800

    add comments to code because Zed said to

--- Learn_Ruby_the_Hard_Way/ex19/ex19.rb
+++ Learn_Ruby_the_Hard_Way/ex19/ex19.rb
@@ -1,3 +1,5 @@
+
+# this is the function called later in the script
 def cheese_and_crackers(cheese_count, boxes_of_crackers)
   puts "You have #{cheese_count} cheeses!"
   puts "You have #{boxes_of_crackers} boxes of crackers!"
@@ -6,20 +8,31 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers)
 end
 
 
+# we can pass in data types (enclosed in objects in Ruby)
 puts "We can give the function numbers directly:"
 cheese_and_crackers(20, 30)
 
 
+# We can indirectly pass them with a variable from our script.
+# This is useful because we can change all function calls that are passed
+# this argument with only a single adjustnment to the code. Diabolical!
 puts "OR, we can just use variables from our script:"
 amount_of_cheese = 10
 amount_of_crackers = 50
 
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
-
+# we can even pass whole functions in as arguments!!! 
+# Or Objects, for that matter.
+# In ruby, primitive data types are not exposed, 
+# but always wrapped in an object.
+# So no matter what, you only ever pass objects and their methods.
 puts "We can even do math inside too:"
 cheese_and_crackers(10 + 20, 5 + 6)
 
-
+# this can get as convoluted as we like, but it's better to do this 
+# kind of stuff someplace other than in the method's parameter.
+# It shoudl be cleanly abstracted to get the jist 
+# without getting lost in the weeds.
 puts "And we can combine the two, vairables and math:"
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
