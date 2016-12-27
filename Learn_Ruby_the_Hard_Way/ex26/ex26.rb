74d5112c3987e939f2fe8fe3df585f90df53b8fe
IanDCarroll <ian.d.carroll@gmail.com>
2016-12-26 20:10:52 -0800

    change Ex25 to Ex26 to reflect reality

--- Learn_Ruby_the_Hard_Way/ex26/ex26.rb
+++ Learn_Ruby_the_Hard_Way/ex26/ex26.rb
@@ -1,46 +1,46 @@
-module Ex25
+module Ex26
 
   # This function will break up words for us.
-  def Ex25.break_words(stuff)
+  def Ex26.break_words(stuff)
     words = stuff.split(' ')
     return words
   end
 
   # Sorts the words.
-  def Ex25.sort_words(words)
+  def Ex26.sort_words(words)
     return words.sort
   end
 
   # Prints the first word after shifting it off.
-  def Ex25.print_first_word(words)
+  def Ex26.print_first_word(words)
     word = words.shift
     puts word
   end
 
   # Prints the last word after popping it off.
-  def Ex25.print_last_word(words)
+  def Ex26.print_last_word(words)
     word = words.pop
     puts word
   end
 
   # Takes in a full sentence and returns the sorted words.
-  def Ex25.sort_sentence(sentence)
-    words = Ex25.break_words(sentence)
-    return Ex25.sort_words(words)
+  def Ex26.sort_sentence(sentence)
+    words = Ex26.break_words(sentence)
+    return Ex26.sort_words(words)
   end
 
   # Prints the first and last words of the sentence.
-  def Ex25.print_first_and_last(sentence)
-    words = Ex25.break_words(sentence)
-    Ex25.print_first_word(words)
-    Ex25.print_last_word(words)
+  def Ex26.print_first_and_last(sentence)
+    words = Ex26.break_words(sentence)
+    Ex26.print_first_word(words)
+    Ex26.print_last_word(words)
   end
 
   # Sorts the words then prints the first and last one.
-  def Ex25.print_first_and_last_sorted(sentence)
-    words = Ex25.sort_sentence(sentence)
-    Ex25.print_first_word(words)
-    Ex25.print_last_word(words)
+  def Ex26.print_first_and_last_sorted(sentence)
+    words = Ex26.sort_sentence(sentence)
+    Ex26.print_first_word(words)
+    Ex26.print_last_word(words)
   end
 
 end
