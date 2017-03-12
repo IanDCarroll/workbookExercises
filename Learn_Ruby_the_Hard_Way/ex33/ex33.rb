595accb7db91a5d5593da8c028f29d8524d28a9b
IanDCarroll <ian.d.carroll@gmail.com>
2017-03-11 22:08:22 -0800

    convert ex33 to a function

--- Learn_Ruby_the_Hard_Way/ex33/ex33.rb
+++ Learn_Ruby_the_Hard_Way/ex33/ex33.rb
@@ -1,17 +1,21 @@
-i = 0
-numbers = []
+def whileCounter(stopping_point)
+  i = 0
+  numbers = []
 
-while i < 6
-  puts "At the top i is #{i}"
-  numbers.push(i)
+  while i < stopping_point
+    puts "At the top i is #{i}"
+    numbers.push(i)
 
-  i += 1
-  puts "Numbers now: ", numbers
-  puts "At the bottom i is #{i}"
+    i += 1
+    puts "Numbers now: ", numbers
+    puts "At the bottom i is #{i}"
+  end
+
+
+  puts "The numbers: "
+
+  # remember you can write this 2 other ways?
+  numbers.each {|num| puts num }
 end
 
-
-puts "The numbers: "
-
-# remember you can write this 2 other ways?
-numbers.each {|num| puts num }
+whileCounter(6)
