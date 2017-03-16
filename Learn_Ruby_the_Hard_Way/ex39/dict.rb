a204d0a46276688c18dc0980d527eabd144319d3
IanDCarroll <ian.d.carroll@gmail.com>
2017-03-16 12:34:47 -0700

    add aDict.rb to Lean Ruby The Hard Way ex39

---
+++ Learn_Ruby_the_Hard_Way/ex39/dict.rb
@@ -0,0 +1,10 @@
+module Dict
+  def Dict.new(num_buckets=256)
+    # Initializes a Dict with the given number of buckets.
+    aDict = []
+    (0...num_buckets).each do |i|
+       aDict.push([])
+    end
+
+    return aDict
+  end
