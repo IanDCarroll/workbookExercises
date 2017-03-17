f419dff636e48b15ab07aec884e360d8e1c01efc
IanDCarroll <ian.d.carroll@gmail.com>
2017-03-17 11:25:16 -0700

    add more of homemade hash to ex39 Learn Ruby THW

--- Learn_Ruby_the_Hard_Way/ex39/dict.rb
+++ Learn_Ruby_the_Hard_Way/ex39/dict.rb
@@ -8,3 +8,30 @@ module Dict
 
     return aDict
   end
+
+  def Dict.hash_key(aDict, key)
+    # Given a key this will create a number and then convert it to
+    # an index for the aDict's buckets.
+    return key.hash % aDict.length
+  end
+
+  def Dict.get_bucket(aDict, key)
+    # given a key, find the bucket where it would go.
+    bucket_id = Dict.hash_key(aDict, key)
+    return aDict[bucket_id]
+  end
+
+  def Dict.get_slot(aDict, key, default=nil)
+    # Returns the index, key, and value of a slot found in a bucket.
+    bucket = Dict.get_bucket(aDict, key)
+
+    bucket.each_with_index do |kv, i|
+      k, v = kv
+      if key == k
+        return i, k, v
+      end
+    end
+
+    return -1, key, default
+  end
+
