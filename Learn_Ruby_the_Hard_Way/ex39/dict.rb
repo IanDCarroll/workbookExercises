c0ca3180758b8a5886f86fc74eddd302eecd9ce9
IanDCarroll <ian.d.carroll@gmail.com>
2017-03-17 13:43:21 -0700

    complete dict.rb for ex39 Learn Ruby the Hard Way

--- Learn_Ruby_the_Hard_Way/ex39/dict.rb
+++ Learn_Ruby_the_Hard_Way/ex39/dict.rb
@@ -35,3 +35,37 @@ module Dict
     return -1, key, default
   end
 
+  def Dict.set(aDict, key, value)
+    # Sets the key to the value, replacing any existing value.
+    bucket = Dict.get_bucket(aDict, key)
+    i, k, v = Dict.get_slot(aDict, key)
+
+    if i >= 0
+      bucket[i] = [key, value]
+    else
+      bucket.push([key, value])
+    end
+  end
+
+  def Dict.delete(aDict, key)
+    # deletes the given key from the Dict.
+    bucket = Dict.get_bucket(aDict, key)
+
+    (0...bucket.length).each do |i|
+      k, v = bucket[i]
+      if key == k
+        bucket.delete_at(i)
+        break
+      end
+    end
+  end
+
+  def Dict.list(aDict)
+    # Prints out what's in the Dict.
+    aDict.each do |bucket|
+      if bucket
+        bucket.each { |k, v| puts k, v}
+      end
+    end
+  end
+end
