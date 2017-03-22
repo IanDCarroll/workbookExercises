3f7506c9e83ee8850a3e9b73dee16c52a73e67bf
IanDCarroll <ian.d.carroll@gmail.com>
2017-03-21 22:10:20 -0700

    delete comments in ex39 Learn Ruby the Hard Way

--- Learn_Ruby_the_Hard_Way/ex39/dict.rb
+++ Learn_Ruby_the_Hard_Way/ex39/dict.rb
@@ -1,9 +1,5 @@
 module Dict
   def Dict.new(num_buckets=256)
-    # Initializes a new Dict object with variable 'aDict'.
-    # aDict is an empty 2-Dimensional array 256 x 0 in dimension.
-    # Each bucket is an empty array inside the greater array 
-    # meant to house a single key-value pair.
     aDict = []
     (0...num_buckets).each do |i|
        aDict.push([])
@@ -13,28 +9,18 @@ module Dict
   end
 
   def Dict.hash_key(aDict, key)
-    # .hash is a built-in ruby function that converts a string to a number.
-    # That number is then modulated down until it is within the limits of the array.
-    # This is then used as an index for the key-value pair in the array.
     return key.hash % aDict.length
   end
 
   def Dict.get_bucket(aDict, key)
-    # Uses hash_key to access an index in the array 
-    # where the key-value pair is/will be stored.
     bucket_id = Dict.hash_key(aDict, key)
     return aDict[bucket_id]
   end
 
   def Dict.get_slot(aDict, key, default=nil)
-    # Returns the index, key, and value of a slot found in a bucket.
-    # Basically it returns every value in the bucket.
-    # This is computationally a lot more efficient than having to check the entire dict.
     bucket = Dict.get_bucket(aDict, key)
 
-    # .each_with_index creates two variables, what the contents are, and what the index is.
     bucket.each_with_index do |kv, i|
-      # this unpacks the bucket(AKA: array) that was indexed.
       k, v = kv
       if key == k
         return i, k, v
@@ -45,13 +31,11 @@ module Dict
   end
 
   def Dict.get(aDict, key, default=nil)
-    #gets the value in a bucket for the given key, or the default.
     i, k, v = Dict.get_slot(aDict, key, default=default)
     return v
   end
 
   def Dict.set(aDict, key, value)
-    # Sets the key to the value, replacing any existing value.
     bucket = Dict.get_bucket(aDict, key)
     i, k, v = Dict.get_slot(aDict, key)
 
@@ -63,7 +47,6 @@ module Dict
   end
 
   def Dict.delete(aDict, key)
-    # deletes the given key from the Dict.
     bucket = Dict.get_bucket(aDict, key)
 
     (0...bucket.length).each do |i|
@@ -76,7 +59,6 @@ module Dict
   end
 
   def Dict.list(aDict)
-    # Prints out what's in the Dict.
     aDict.each do |bucket|
       if bucket
         bucket.each { |k, v| puts k, v}
