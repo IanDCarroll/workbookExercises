663296a82d8cd10e9802e2934d1d16abd1889bae
IanDCarroll <ian.d.carroll@gmail.com>
2017-03-22 13:03:15 -0700

    write a first ruby class in ex40

---
+++ Learn_Ruby_the_Hard_Way/ex40/ex40b.rb
@@ -0,0 +1,19 @@
+class MyStuff
+
+  def initialize()
+    @tangerine = "And now a thousand years between"
+  end
+
+  attr_reader :tangerine
+
+  def apple()
+    puts "I AM CLASSY APPLES!"
+  end
+
+end
+
+thing = MyStuff.new()
+thing.apple()
+# ruby class variables use . 
+# ruby module global variables use ::
+puts thing.tangerine
