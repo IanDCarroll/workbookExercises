b5d7adceed79a7b487367a9b2c1e3ddc2c0402ad
IanDCarroll <ian.d.carroll@gmail.com>
2017-03-26 12:55:03 -0700

    comment as Zed wishes for ex42 Learn Ruby THW

--- Learn_Ruby_the_Hard_Way/ex42/ex42.rb
+++ Learn_Ruby_the_Hard_Way/ex42/ex42.rb
@@ -2,85 +2,87 @@
 class Animal
 end
 
-## ??
+## Dog is-an Animal
 class Dog < Animal
 
   def initialize(name)
-    ## ??
+    ## A dog has-a name
     @name = name
   end
 end
 
-## ??
+## Cat is-an Animal
 class Cat < Animal
 
   def initialize(name)
-    ## ??
+    ## a Cat has-a name
     @name = name
   end
 end
 
-## ??
+## Person is-an Object (Not an Animal (In this implementation))
 class Person
 
   def initialize(name)
-    ## ??
+    ## A Person has-a name
     @name = name
 
-    ## Person has-a pet of some kind
+    ## Person has-a pet of some kind too
     @pet = nil
   end
 
+  # allows pet to be read and written to
   attr_accessor :pet
 end
 
-## ??
+## An Employee is-a Person
 class Employee < Person
 
   def initialize(name, salary)
-    ## ?? hmm what is this strange magic?
+    ## Allows Employee to use its parent's constructor
     super(name)
-    ## ??
+    ## An Employee is a special kind of Person that has-a salary
     @salary = salary
   end
 
 end
 
-## ??
+## A Fish is-an Object
 class Fish
 end
 
-## ??
+## A Salmon is-a Fish
 class Salmon < Fish
 end
 
-## ??
+## A Halibut is-a Fish
 class Halibut < Fish
 end
 
-## rover is-a dog
+## rover is-a dog instance
 rover = Dog.new("Rover")
 
-## ??
+## satan is-a Cat instance
 satan = Cat.new("Satan")
 
-## ??
+## Mary is-a person insance
 mary = Person.new("Mary")
 
-## ??
+## Mary has-a pet atrtribute, which is set to satan here
 mary.pet = satan
 
-## ??
+## Frank is-an Employee instance
 frank = Employee.new("Frank", 120000)
 
-## ??
+## Frank has-a pet attribute, which is set to rover here
 frank.pet = rover
 
-## ??
+## Flipper is a Fish instance 
+#  (Not a dolphin instance, which is-a Mammal, not a Fish)
 flipper = Fish.new()
 
-## ??
+## Crouse is a Salmon instance
 crouse = Salmon.new()
 
-## ??
+## Harry is a Halibut instance
 harry = Halibut.new()
