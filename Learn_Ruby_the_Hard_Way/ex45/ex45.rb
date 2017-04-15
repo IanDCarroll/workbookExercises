06112ed23390ba89aefb75a35a6d238afefe3891
IanDCarroll <ian.d.carroll@gmail.com>
2017-04-14 23:16:51 -0700

    finish ex45 Learn Ruby THW

--- Learn_Ruby_the_Hard_Way/ex45/ex45.rb
+++ Learn_Ruby_the_Hard_Way/ex45/ex45.rb
@@ -20,24 +20,130 @@ end
 
 class Begin < Scene
   def enter
-    puts "You wake up and die immediately."
-    return 'loss'
+    puts "You wake up and have to figure out what to wear."
+    print_choices("pants", "kilt")
+    
+    choice = get_player_choice
+
+    if choice == '1'
+      puts "You put on some pants and head out."
+      puts ""
+      return 'metro'
+    elsif choice == '2'
+      puts "You head out with your kilt on."
+      puts "It's windy today."
+      return 'loss'
+    else
+      error
+      return 'begin'
+    end
   end
 end
 
 class Metro < Scene
+  def enter 
+    puts "You are riding on the metro."
+    print_choices("Remain standing","Take a seat")
+
+    choice = get_player_choice
+
+    if choice == '1'
+      puts "You are still awake when your stop comes up."
+      puts ""
+      return 'aikido'
+    elsif choice == '2'
+      puts "You wake up in Long Beach."
+      return 'loss'
+    else
+      error
+      return 'metro'
+    end
+  end
 end
 
 class Aikido < Scene
+  def enter
+    puts "You show up at the Aikido dojo."
+    print_choices("Pay attention", "Just go with the flow")
+
+    choice = get_player_choice
+
+    if choice == '1'
+      puts "When you're thrown you roll around on the ground. It's fun."
+      puts ""
+      return '8th_light'
+    elsif choice == '2'
+      puts "You are surprised to be thrown to the ground and pull a muscle."
+      return 'loss'
+    else
+      error
+      return 'aikido'
+    end
+  end
 end
 
 class EighthLight < Scene
+  def enter
+    puts "You start you day's work at 8th Light as a new apprentice."
+    print_choices("Work hard & learn stuff", "Eat the old salad in the fridge")
+
+    choice = get_player_choice
+
+    if choice == '1'
+      puts "You learn all kinds of superpowers and everyone loves you."
+      puts ""
+      return 'D&D'
+    elsif choice == '2'
+      puts "The salad tastes bad and everyone silently judges you."
+      return 'loss'
+    else
+      error
+      return '8th_ligth'
+    end
+  end
 end
 
 class DandD < Scene
+  def enter
+    puts "You meet friends from the book store to play Dungeons and Dragons."
+    print_choices("Order pizza", "Eat nothing all day except jolly ranchers")
+
+    choice = get_player_choice
+
+    if choice == '1'
+      puts "Everyone has fun murdering imaginary monsters in madcap scenarios."
+      puts ""
+      return 'sleep'
+    elsif choice == '2'
+      puts "You crash hard convinced you flash-gave yourself type II diabetes."
+      return 'loss'
+    else
+      error
+      return 'D&D'
+    end
+  end
 end
 
 class Sleep < Scene
+  def enter
+    puts "You somehow defy space and time and get home before midnight."
+    print_choices("Get your pants out for tomorrow", "Sleep immediately")
+
+    choice = get_player_choice
+
+    if choice == '1'
+      puts "You have pants ready so you can do all this again tomorrow."
+      puts ""
+      return 'win'
+    elsif choice == '2'
+      puts "You can't find your pants next morning and have to wear a kilt."
+      puts "It's windy."
+      return 'loss'
+    else
+      error
+      return 'sleep'
+    end
+  end
 end
 
 class Loss < Scene
@@ -57,6 +163,11 @@ end
 class Map
   @@scenes = {
     'begin' => Begin.new,
+    'metro' => Metro.new,
+    'aikido' => Aikido.new,
+    '8th_light' => EighthLight.new,
+    'D&D' => DandD.new,
+    'sleep' => Sleep.new,
     'loss' => Loss.new,
     'win' => Win.new
   }
--- Learn_Ruby_the_Hard_Way/ex45/scene_class.rb
+++ Learn_Ruby_the_Hard_Way/ex45/scene_class.rb
@@ -5,4 +5,18 @@ class Scene
     puts "so this is the end."
     exit(1)
   end
+
+  def print_choices(choice1, choice2)
+    puts "\t1. #{choice1}"
+    puts "\t2. #{choice2}"
+  end
+
+  def get_player_choice
+    print "> "
+    return $stdin.gets.chomp
+  end
+
+  def error
+    puts "Sorry that's not a 1 or a 2. Try again."
+  end
 end
