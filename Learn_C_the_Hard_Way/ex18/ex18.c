1fd684bea8d6f161149a7ed90ced981a9e575719
Ian Carroll <ian.d.carroll@gmail.com>
2017-10-12 17:21:18 -0700

    add function declarations and bring in required libraries

--- Learn_C_the_Hard_Way/ex18/ex18.c
+++ Learn_C_the_Hard_Way/ex18/ex18.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+
+typedef int (*compare_cb) (int a, int b);
+int *bubble_sort(int *numbers, int count, compare_cb cmp);
+void test_sorting(int *numbers, int count, compare_cb cmp);
+void die(const char *message);
 
 int main(int argc, char *argv[]) {
    return 0;
